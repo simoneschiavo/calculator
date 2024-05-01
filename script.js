bbc5274c00d99301ceb4a10315c8764e62d22931
Simone Schiavo <simoneschiavo@icloud.com>
2024-05-02 00:28:46 +0200

    Store keys as variables

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-const keys = document.querySelectorAll("button");
-const screen = document.querySelector(".input-result");
-
-keys.forEach((key) => {
-    key.addEventListener("click", e => {
-            screen.textContent += e.target.innerText;
-    });
-});
-
-// const updateScreen = function (e) {
-//     screen.textContent += e.target.innerText;
-// }
-
-// cancel.addEventListener("click", e => {
-//     console.log(e.target.innerText);
-// })
+// Store variables
+const input = document.querySelector(".input-result");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operation");
+const clear = document.querySelector(".cancel");
+const back = document.querySelector(".back");
+const pctg = document.querySelector(".pctg");
