60fd29a78cab39bf33927e7787ad9aa0c7f52b34
Simone Schiavo <simoneschiavo@icloud.com>
2024-05-02 09:09:32 +0200

    Add integer operations
    It's now possible to calculate operations between integers, also in
    series (example 1 * 5 = 5 + 3 = 8 + 12 = 20)

--- script.js
+++ script.js
@@ -5,3 +5,55 @@ const operators = document.querySelectorAll(".operation");
 const clear = document.querySelector(".cancel");
 const back = document.querySelector(".back");
 const pctg = document.querySelector(".pctg");
+
+let firstNum = "";
+let secondNum = "";
+let operation = "";
+let result = "";
+
+// Store variables from clicks
+numbers.forEach(number => {
+    number.addEventListener("click", e => {
+        if (operation === "") {
+            firstNum += e.target.innerText;
+        } else {
+            secondNum += e.target.innerText;
+        };
+
+        input.textContent += e.target.innerText;
+    });
+});
+
+operators.forEach(operator => {
+    operator.addEventListener("click", e => {
+        if (e.target.innerText !== "=") {
+            operation = e.target.innerText;
+            input.textContent += e.target.innerText;
+        } else {
+            operate();
+        };
+    });
+});
+
+// Create the operate() function
+function operate() {
+    switch (operation) {
+        case "+":
+            result = parseInt(firstNum) + parseInt(secondNum);
+            break;
+        case "-":
+            result = parseInt(firstNum) - parseInt(secondNum);
+            break;
+        case "x":
+            result = parseInt(firstNum) * parseInt(secondNum);
+            break;
+        case "/":
+            result = parseInt(firstNum) / parseInt(secondNum);
+            break;
+        default:
+            break;
+    };
+    input.textContent = result;
+    firstNum = result;
+    secondNum = "";
+};
